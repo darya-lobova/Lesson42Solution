da561ff704bf6cc8244235de7b1b26bfcccae897
darya-lobova <dasha.lobova.it@gmail.com>
2026-02-18 18:50:54 +0300

    Task 04 solved

---
+++ Task04/logic.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#define DEFAULT_SIZE 100
+using namespace std;
+
+int max(int array[DEFAULT_SIZE], int length);
+int min(int array[DEFAULT_SIZE], int length);
+double arithmetical_average(int array[DEFAULT_SIZE], int length);
+double geometrical_average(int array[DEFAULT_SIZE], int length);
